254ffa044d3fc3231f4c2dc6552a89056bea9467
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-14 19:22:50 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0873b46c45c11058d"
   instance_type = "t2.large"
   associate_public_ip_address = "true"
-  key_name      = "${aws_key_pair.class.key_name}"
+  key_name      = "${aws_key_pair.oregonkey.key_name}"
   #user_data     = "${file("userdata.sh")}"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   availability_zone = "us-west-2"
