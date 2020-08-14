6b8a75c6213f3cebabc19f3e008c86b28f57866e
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-14 19:49:21 -0400

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -1,11 +1,13 @@
-resource "aws_ebs_volume" "web" {
+resource "aws_ebs_volume" "ebs" {
     availability_zone = "us-west-2"
     size = 100
 }
 
 resource "aws_volume_attachment" "ebs_att" {
-    device_name = "/dev/sdc"
-    volume_id = "${aws_ebs_volume.web.id}"
+    device_name = "/dev/sdh"
+    volume_id = "${aws_ebs_volume.ebs.id}"
     instance_id = "${aws_instance.web.id}"
 }
+
+
 
