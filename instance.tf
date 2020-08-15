3d352b51f51331022b42f5f12028f6a55b2f1b36
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-14 20:11:50 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   #count = 10
-  ami           = "aami-0873b46c45c11058d"
+  ami           = "ami-0873b46c45c11058d"
   instance_type = "t2.large"
   associate_public_ip_address = "true"
   key_name      = "${aws_key_pair.oregonkey.key_name}"
