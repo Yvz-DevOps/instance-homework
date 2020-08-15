11bdf8d258f3e46c68687d94ac426ff483938586
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-14 20:22:20 -0400

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -9,6 +9,18 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_http_https" {
+  name        = "allow_http_https"
+  description = "Allow TLS inbound traffic"
   ingress {
     description = "Allow HTTPS"
     from_port   = 443
